26e1e8b4f683410d8c122dfe16d8c74ef3cab939
DongWooKim97 <nanaukim@naver.com>
2022-10-24 22:31:12 +0900

    refactor: add success or fail

--- src/play/getScore.js
+++ src/play/getScore.js
@@ -5,7 +5,7 @@ export default function getScore(computerInputNumbers, userInputNumbers) {
   const strikeCount = getStrikeCount(computerNumArr, userNumArr);
   const ballCount = getBallCount(computerNumArr, userNumArr);
 
-  return `${ballCount}볼 ${strikeCount}스트라이크`
+  return scoreCheck(strikeCount, ballCount);
 
 }
 
@@ -20,11 +20,30 @@ function getStrikeCount(computerNumArr, userNumArr) {
 
 function getBallCount(computerNumArr, userNumArr) {
   return userNumArr.reduce((acc, currentValue, index) => {
-    if (computerNumArr[index] !== currentValue
-       && computerNumArr.includes(currentValue)) {
+    if (computerNumArr[index] !== currentValue &&
+      computerNumArr.includes(currentValue)) {
       return acc + 1;
     }
     return acc
   }, 0)
 
 };
+
+function scoreCheck(strikeCount, ballCount) {
+  if (strikeCount === 3) {
+    return `정답`
+  }
+  if (!strikeCount && !ballCount) {
+    return '낫싱'
+  }
+  if (!strikeCount && ballCount) {
+    return `${ballCount}볼`
+  }
+  if (strikeCount && !ballCount) {
+    return `${strikeCount}스트라이크`
+  }
+  if(strikeCount && ballCount) {
+    return `${ballCount}볼 ${strikeCount}스트라이크`
+  }
+
+}

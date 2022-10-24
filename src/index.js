b9c0d1edb9ffe8a6248950468e9bbf07e6bb3eb6
DongWooKim97 <nanaukim@naver.com>
2022-10-24 18:43:10 +0900

    feat: init baseball game(test)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default function BaseballGame() {
+  this.play = function (computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  };
+
+  console.log('게임 시작 함수 생성')
+
+}
+
+new BaseballGame();

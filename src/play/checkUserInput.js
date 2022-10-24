6674f42d255c3c85cf3471b338c9ac00c0cb364b
DongWooKim97 <nanaukim@naver.com>
2022-10-24 23:24:09 +0900

    refactor: can path what number correct is

--- src/play/checkUserInput.js
+++ src/play/checkUserInput.js
@@ -1,10 +1,17 @@
 import getScore from "./getScore.js"
+import userInputValid from "./userInputValid.js";
 
 export default function checkUserInput(computerInputNumbers){
   
   submit.addEventListener('click', () => {
     const userInputNumbers = document.getElementById('user-input').value;
 
-    document.getElementById('result').innerHTML = getScore(computerInputNumbers, userInputNumbers)
+    if(userInputValid(userInputNumbers)){
+      document.getElementById('result').innerHTML = getScore(computerInputNumbers, userInputNumbers)
+    }  else{
+      console.log('실패야실패')
+    }
+
+
   })
 }

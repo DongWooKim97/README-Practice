14659a96275416519b555066a810b3da09e042e8
DongWooKim97 <nanaukim@naver.com>
2022-10-25 16:52:07 +0900

    style: change logical flow

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import getComputerInput from "./input/getComputerInput.js";
+import playGame from "./play/playGame.js"
 // import handler from "./play/handler.js";
-import checkUserInput from "./play/checkUserInput.js";
 
 export default function BaseballGame() {
   const computerInputNumbers = getComputerInput();
   // const submit = document.getElementById('submit')
   console.log(computerInputNumbers);
   // for score
-  checkUserInput(computerInputNumbers)
-
+  
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    return ;
   };    
-
+  
+  playGame(computerInputNumbers, this.play)
 }
 
 new BaseballGame();

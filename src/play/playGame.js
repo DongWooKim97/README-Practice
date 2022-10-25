6ac825a958f21b1bd79f419bd6d8c493eef01cfa
DongWooKim97 <nanaukim@naver.com>
2022-10-25 17:03:24 +0900

    style: remove use code and cahnge logical flow

--- src/play/playGame.js
+++ src/play/playGame.js
@@ -1,35 +1,36 @@
 import getScore from "./getScore.js"
-import userInputValid from "./userInputValid.js";
+import userInputValid from "../input/userInputValid.js";
 import inputRestart from "../input/inputRestart.js";
 import getComputerInput from "../input/getComputerInput.js";
 
-function resetScreen(){
+function resetScreen() {
   document.querySelector('#result').textContent = "";
-  document.querySelector('#user-input').value= "";
+  document.querySelector('#user-input').value = "";
 }
 
-export default function checkUserInput(computerInputNumbers){
+export default function playGame(computerInputNumbers, play) {
   const $submit = document.getElementById('submit');
   const $result = document.getElementById('result');
 
   $submit.addEventListener('click', () => {
     const userInputNumbers = document.getElementById('user-input').value;
-    const resultDomain = document.getElementById('result')
+    // const resultDomain = document.getElementById('result')
 
-    if(userInputValid(userInputNumbers)){
-      inputRestart(resultDomain,getScore(computerInputNumbers, userInputNumbers))  
-    }  else{
-      console.log('실패야실패')
+    if (userInputValid(userInputNumbers)) {
+      // inputRestart(resultDomain, getScore(computerInputNumbers, userInputNumbers))
+      inputRestart(play(computerInputNumbers, userInputNumbers))
     }
+
   })
 
-  $result.addEventListener('click', ({target}) => {
-    if(target.id === 'game-restart-button') {
+  $result.addEventListener('click', ({
+    target
+  }) => {
+    if (target.id === 'game-restart-button') {
       resetScreen()
       console.log('1');
       computerInputNumbers = getComputerInput();
-      console.log('3',computerInputNumbers);
+      console.log('3', computerInputNumbers);
     }
   })
 }
-

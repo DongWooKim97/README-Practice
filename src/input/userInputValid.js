a543063fbc4f871e7ea5cddf7e356b7f35334ead
DongWooKim97 <nanaukim@naver.com>
2022-10-25 17:02:46 +0900

    style: remove useless code

--- src/input/userInputValid.js
+++ src/input/userInputValid.js
@@ -1,19 +1,3 @@
-import inputErrorAlert from "../input/inputErrorAlert.js";
-
-// function correctWord(userInputNumbers) {
-//   if (userInputNumbers.length !== 3) {
-//     return inputErrorAlert('lengthError');
-//   }
-//   if (!Number.isInteger(+userInputNumbers)) {
-//     return inputErrorAlert('inputTypeError');
-//   }
-//   if (userInputNumbers.length !== [...new Set(userInputNumbers)].length) {
-//     return inputErrorAlert('duplicateError');
-//   }
-//   return true
-// }
-
-
 export default function userInputValid(userInputNumbers) {
   userInputNumbers = userInputNumbers.split('').map(i => +i);
   if (!checkInputType(userInputNumbers)) {
